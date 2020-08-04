b5d542f599e45a2b731ae529d7c2c6dc13f288d7
Meng Zhou <zhoumeng122@gmail.com>
2020-08-04 15:42:34 -0700

    Initial commit

---
+++ src/pages/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home_view(request, *args, **kwargs):
+    #return HttpResponse("<h1>Hello World</h1>") #string of html code
+    return render(request, "home.html", {})
+
+def contact_view(request, *arg, **kwarg):
+    #return HttpResponse("<h1>Contact Page</h1>")
+    return render(request, "contact.html", {})
+
+def about_view(request, *arg, **kwarg):
+    my_context = {
+        'my_text': 'This is about us',
+        'my_number': 123,
+        'my_list':[1345,245,34, 'ABC']
+    }
+    return render(request, "about.html", my_context)
